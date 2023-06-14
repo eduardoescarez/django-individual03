04517e7fa3d047adcc435fdf4df67639f9116b7f
Eduardo Escárez <eduardoe@gmail.com>
2023-06-13 20:56:52 -0400

    Punto inicial de la app

---
+++ website/mainapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class IndexView(TemplateView): 
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'titulo': 'Somos WebConstructores, tú solución de desarrollo web'},)
